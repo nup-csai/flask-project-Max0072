036ce10825ab2424b9befff085b45af9974e6860
Max0072 <maks.shikunov2006@gmail.com>
2024-11-12 13:51:06 +0200

    structure ready to deploy

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 from flask import Flask
-
+import json
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "inferior task tracker"
+
+
+@app.route("/tasks")
+def tasks():
+    data = json.load(open("tasks.json"))
+    x = data[0]("task")
+    return x
